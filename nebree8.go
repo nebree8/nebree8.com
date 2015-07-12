23bd24ee7b62d4894ff5d32bd117e4f544a0d288
Sagar Mittal <sagar@sagarmittal.com>
2015-07-11 17:26:02 -0700

    Skeleton

---
+++ nebree8.go
@@ -0,0 +1,14 @@
+package nebree8
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func init() {
+  http.HandleFunc("/api/hello", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, "Hello World!")
+}
