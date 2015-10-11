800cf1ec5fdc0f62d4d7780d251c9c8fc2a85749
Sagar Mittal <sagar@sagarmittal.com>
2015-10-10 19:47:44 -0700

    Move name entry dialog template out of JS

--- static/drink-detail/drink-detail.js
+++ static/drink-detail/drink-detail.js
@@ -55,25 +55,7 @@ angular.module('nebree8.drink-detail', [])
           locals: {
             user_name: $scope.user_name
           },
-          template: '<md-dialog aria-label="Enter your name">' +
-            ' <form ng-submit="closeDialog()"> ' +
-            ' <md-toolbar><div class="md-toolbar-tools"> ' +
-            '  <h2>What should we call you?</h2> ' +
-            '  <span flex></span><md-button class="md-icon-button" ' +
-            '      ng-click="cancel()"><md-icon md-svg-src="img/icons/ic_close_24px.svg" aria-label="Close dialog"></md-icon></md-button> ' +
-            ' </div></md-toolbar> ' +
-            ' <md-dialog-content>' +
-            '   <md-input-container><label>Your Name</label> ' +
-            '   <input type="text" ng-model="user_name" focus-on="dialogOpened"> ' +
-            '   </md-input-container> ' +
-            ' </md-dialog-content>' +
-            ' <div class="md-actions"> ' +
-            '  <md-button ng-click="cancel()">Cancel</md-button> ' +
-            '  <md-button type="submit" ng-disabled="!user_name" class="md-primary"> ' +
-            '      Make Drink</md-button> ' +
-            ' </div> ' +
-            ' </form> ' +
-            '</md-dialog>'
+          templateUrl: 'drink-detail/enter-name-dialog.html',
         })
         .then(function(answer) {
           console.log("answer", answer);
