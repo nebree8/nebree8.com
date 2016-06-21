78ff92b4383f62996f2eb9300c77915d492d9792
Travis Briggs <audiodude@gmail.com>
2016-06-20 21:01:39 -0700

    Hide order-status so that bottom button can be clicked.
    Also add animation to page-about. Both of these changes require an AboutCtrl.

--- static/about/about.ts
+++ static/about/about.ts
@@ -1,8 +1,16 @@
+class AboutCtrl {
+  pageClass = 'page-about';
+  hideOrderStatus = true;
+}
+
 angular.module('nebree8.about', [])
   .config(['$routeProvider',
     function($routeProvider: angular.route.IRouteProvider) {
       $routeProvider.when('/about', {
         templateUrl: 'about/about.html',
+        controller: 'AboutCtrl'
+        controllerAs: 'ctrl',
       });
     }
-  ]);
+  ])
+  .controller('AboutCtrl', AboutCtrl);
