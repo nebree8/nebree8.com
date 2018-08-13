fa78588efefa0800098f3d92ca84fa7fc1e330a0
Sagar Mittal <sagar@sagarmittal.com>
2018-08-12 23:24:12 -0700

    Use postcss to w/ nested, autoprefixer plugins to simplify CSS

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@ var concat = require('gulp-concat');
 var csso = require('gulp-csso');
 var minify = require('gulp-minify');
 var ngAnnotate = require('gulp-ng-annotate');
+var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var ts = require('gulp-typescript');
 var uglify = require('gulp-uglify');
 
 var config = {
   htmlGlob: 'ng-app/**/*.html',
-  cssGlob: 'ng-app/app.css',
+  cssGlob: 'ng-app/**/*.css',
   tsGlob: 'ng-app/**/*.ts',
 };
 
@@ -22,6 +23,7 @@ gulp.task('css', function(){
   return gulp.src(config.cssGlob)
     .pipe(sourcemaps.init())
       .pipe(concat('app.css'))
+      .pipe(postcss([require('postcss-nested'), require('autoprefixer')]))
       .pipe(csso())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('public/'))
